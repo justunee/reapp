fc4bf09be435453a1ba1b6bb4f368fcfad87bc49
justunee <lifeistogive@gmail.com>
2019-03-13 17:41:47 +0700

    props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Tee from './tee';
 
 
 class App extends Component {
+  state ={
+    teelist : [
+        {name:"june",age:"20",belt:'white',id:1},
+        {name:"junee",age:"30",belt:'red',id:2},
+        {name:"just",age:"40",belt:'green',id:3},
+    ]
+  }
   render() {
     return (
       <div className="App">     
           
-          <Tee name="June" age="20" belt="white"/>
+          {/* <Tee name="June" age="20" belt="white"/>
+          <Tee name="junee" age="18" belt="green"/> */}
+          <Tee teelist={this.state.teelist}/>
           
       </div>
     );
--- src/tee.js
+++ src/tee.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 
 class tee extends Component{
-render(){
-const {name,age,belt} = this.props;
+// render(){
+// const {name,age,belt} = this.props;
+// const{teelist} = this.props;
+// const newteelist = teelist.map(cyclist => {
+//     return (
+//         <div className="Tee1">
+//             <div>Name:{cyclist.name}</div>
+//             <div>age:{cyclist.age}</div>
+//             <div>Belt:{cyclist.belt}</div>
+//         </div>
+//             )
+// })
 
-    return(
-        <div className="Tee">
-            <div>Name:{ this.props.name }</div>
-            <div>age:{this.props.age}</div>
-            <div>Belt:{this.props.belt}</div>
-            <div>Name:{name}</div>
-            <div>age:{age}</div>
-            <div>Belt:{belt}</div>
-        </div>
-        )
-    }
+//     return(
+//         <div className="Tee">
+//             <div>Name:{ this.props.name }</div>
+//             <div>age:{this.props.age}</div>
+//             <div>Belt:{this.props.belt}</div>
+//             <div>Name:{name}</div>
+//             <div>age:{age}</div>
+//             <div>Belt:{belt}</div>      
+//             { newteelist }      
+//         </div>       
+
+//         )
+//     }
+
+render(){
+    const{teelist} = this.props;
+    const newteelist = teelist.map(cyclist => {
+        return (
+            <div className="Tee1" key={cyclist.id}>
+                <div>Name:{cyclist.name}</div>
+                <div>age:{cyclist.age}</div>
+                <div>Belt:{cyclist.belt}</div>
+            </div>
+                )
+    })
+    
+        return(
+            <div className="Tee">                   
+                { newteelist }      
+            </div>       
+    
+            )
+        }
 
 }
 
