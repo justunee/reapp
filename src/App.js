26685e69132782751f976f220808274dcbd50132
justunee <lifeistogive@gmail.com>
2019-03-16 17:30:12 +0700

    finish basic react

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
-import Tee from './tee';
-import AddTee from './addTee'
-
+import React, { Component } from "react";
+import Tee from "./tee";
+import AddTee from "./addTee";
 
 class App extends Component {
-  state ={
-    teelist : [
-        {name:"june",age:"20",belt:'white',id:1},
-        {name:"junee",age:"30",belt:'red',id:2},
-        {name:"just",age:"40",belt:'green',id:3},
+  state = {
+    teelist: [
+      { name: "june", age: "20", belt: "white", id: 1 },
+      { name: "junee", age: "30", belt: "red", id: 2 },
+      { name: "just", age: "40", belt: "green", id: 3 }
     ]
-  }
+  };
 
-  addTolist = (listFromAdd) => 
-  {
+  addTolist = listFromAdd => {
     listFromAdd.id = Math.random();
     let TempArray = [...this.state.teelist, listFromAdd];
 
     this.setState({
       teelist: TempArray
-    })
+    });
 
     console.log(listFromAdd);
+  };
+
+  deleteList = id => {
+    let a = this.state.teelist.filter(b => {
+      return b.id !== id;
+    });
+    this.setState({
+      teelist: a
+    });
+
+    console.log(id);
+  };
+
+  componentDidMount(){
+console.log('componet mount');
   }
-
-  deleteList = (id) =>{
-      let a = this.state.teelist.filter(b =>{
-          return b.id !== id
-
-      });
-      this.setState({
-        teelist: a
-
-      })
-
-    console.log(id)
+  componentDidUpdate(prevProps,prevState){
+    console.log("update");
+    console.log(prevProps,prevState);
 
   }
 
   render() {
     return (
-      <div className="App">     
-          
-          {/* <Tee name="June" age="20" belt="white"/>
+      <div className="App">
+        {/* <Tee name="June" age="20" belt="white"/>
           <Tee name="junee" age="18" belt="green"/> */}
-          <Tee PassdeleteList={this.deleteList} teelist={this.state.teelist}/>
-          <AddTee fromAddToList={this.addTolist}/>
+        <Tee PassdeleteList={this.deleteList} teelist={this.state.teelist} />
+        <AddTee fromAddToList={this.addTolist} />
       </div>
     );
   }
