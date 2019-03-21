7921f65b463ccb98af96e48467d43c2fe5f843b3
justunee <lifeistogive@gmail.com>
2019-03-21 21:13:52 +0700

    Todo update

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
 import React, { Component } from "react";
-import Tee from "./tee";
-import AddTee from "./addTee";
+import Todos from './Todo';
 
 class App extends Component {
   state = {
-    teelist: [
-      { name: "june", age: "20", belt: "white", id: 1 },
-      { name: "junee", age: "30", belt: "red", id: 2 },
-      { name: "just", age: "40", belt: "green", id: 3 }
-    ]
+    todo: [{ id: 1, content: "Buy milk" }, { id: 2, content: "play game" }]
   };
-
-  addTolist = listFromAdd => {
-    listFromAdd.id = Math.random();
-    let TempArray = [...this.state.teelist, listFromAdd];
-
-    this.setState({
-      teelist: TempArray
-    });
-
-    console.log(listFromAdd);
-  };
-
-  deleteList = id => {
-    let a = this.state.teelist.filter(b => {
-      return b.id !== id;
-    });
-    this.setState({
-      teelist: a
-    });
-
-    console.log(id);
-  };
-
-  componentDidMount() {
-    console.log("componet mount");
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log("update");
-  }
 
   render() {
-    return (
-      <div className="App">
-        {/* <Tee name="June" age="20" belt="white"/>
-          <Tee name="junee" age="18" belt="green"/> */}
-        <Tee PassdeleteList={this.deleteList} teelist={this.state.teelist} />
-        <AddTee fromAddToList={this.addTolist} />
-      </div>
-    );
+    return (<div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todo}/>
+
+
+    </div>
+    )
   }
 }
 
