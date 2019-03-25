fd58dc072c11bd8690a2128361ad38ccc0f452af
justunee <lifeistogive@gmail.com>
2019-03-25 23:50:17 +0700

    Finished

--- src/addForm.js
+++ src/addForm.js
@@ -12,12 +12,15 @@ class Addtod extends Component{
     handleSubmit = (e)=>{
         e.preventDefault();
         this.props.addTodo(this.state);
+        this.setState({
+            content:''
+        })
     }
     render(){
             return(<div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add New TODO:</label>
-                    <input type ="text" onChange={this.handleChange}/>
+                    <input type ="text" onChange={this.handleChange} value={this.state.content/>
                 </form>
             </div>
             )
